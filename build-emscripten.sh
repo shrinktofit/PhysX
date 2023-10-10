5a735185effbe746a941824a18b382e6265cf858
Ling Zhan <lealzhan@126.com>
2023-10-10 17:42:11 +0800

    update build-emscripten.sh (#16)

--- build-emscripten.sh
+++ build-emscripten.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # emscripten 3.1.41
 
+copy_dest=../../cocos-engine/native/external/emscripten/physx
+
 set -e
 
-echo -e "\033[01;32m --------------- START -------------------- \033[0m"
+echo -e "\033[01;32m |||  START  ||| \033[0m"
 
 get_current_time_in_seconds() {
     local now=$(date +'%Y-%m-%d %H:%M:%S')
@@ -28,45 +30,64 @@ else
     script_suffix='.sh'
 fi
 
-echo "--------|||  BUILD JS  |||--------"
-echo "|||  GENERATE |||"
+cd $base_dir
+mkdir -p $base_dir/builds
+
+# print mode
+echo -e "\033[01;32m |||  MODE: $mode  ||| \033[0m"
+
+#-------------------------asm.js-----------------------------------
+# if mode is release, build asm.js
+if [ $mode == "release" ]; then
+echo -e "\033[01;32m |||  BUILD ASM.JS START  ||| \033[0m"
+echo -e "\033[01;32m |||  GENERATE ||| \033[0m"
+cd $base_dir
 cd physx/
 ./generate_projects${script_suffix} emscripten-js
 
 if [ $? -eq 0 ]; then
-    echo "Generated Project successfully"
+    echo -e "\033[01;32m Generated Project successfully \033[0m"
 else
-    echo "Failed to generate Project"
+    echo -e "\033[01;32m Failed to generate Project \033[0m"
     exit 1
 fi
 
-echo "|||  COMPILE |||"
+echo -e "\033[01;32m |||  COMPILE ||| \033[0m"
 cd compiler/emscripten-js-$mode
-ninja
+# ninja
 
-echo "--------|||  BUILD WASM  |||--------"
+echo -e "\033[01;32m |||  BUILD ASM.JS END  ||| \033[0m"
 cd $base_dir
-echo "|||  GENERATE |||"
+cp $base_dir/physx/bin/emscripten/$mode/physx.$mode.asm.js $base_dir/builds
+cp -r $base_dir/builds/physx.$mode.asm.js $copy_dest
+fi
+
+#--------------------------wasm----------------------------------
+echo -e "\033[01;32m |||  BUILD WASM START  ||| \033[0m"
+echo -e "\033[01;32m |||  GENERATE ||| \033[0m"
+cd $base_dir
 cd physx/
 ./generate_projects${script_suffix} emscripten-wasm
-echo "|||  COMPILE |||"
+
+if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m Generated Project successfully \033[0m"
+else
+    echo -e "\033[01;32m Failed to generate Project \033[0m"
+    exit 1
+fi
+
+echo -e "\033[01;32m |||  COMPILE ||| \033[0m"
 cd compiler/emscripten-wasm-$mode
-ninja
+# ninja
 
-echo "|||  COPY  |||"
+echo -e "\033[01;32m |||  BUILD WASM END  ||| \033[0m"
 cd $base_dir
-mkdir -p $base_dir/builds
-cp $base_dir/physx/bin/emscripten/$mode/physx.$mode.asm.js $base_dir/builds/physx.$mode.asm.js
-cp $base_dir/physx/bin/emscripten/$mode/physx.$mode.wasm.js $base_dir/builds/physx.$mode.wasm.js
-cp $base_dir/physx/bin/emscripten/$mode/physx.$mode.wasm.wasm $base_dir/builds/physx.$mode.wasm.wasm
+cp $base_dir/physx/bin/emscripten/$mode/physx.$mode.wasm.js $base_dir/builds
+cp $base_dir/physx/bin/emscripten/$mode/physx.$mode.wasm.wasm $base_dir/builds
+cp -r $base_dir/builds/physx.$mode.wasm.js $copy_dest
+cp -r $base_dir/builds/physx.$mode.wasm.wasm $copy_dest
 
-cp -r $base_dir/builds/physx.$mode.asm.js ../../cocos-engine/native/external/emscripten/physx/physx.$mode.asm.js
-cp -r $base_dir/builds/physx.$mode.wasm.js ../../cocos-engine/native/external/emscripten/physx/physx.$mode.wasm.js
-cp -r $base_dir/builds/physx.$mode.wasm.wasm ../../cocos-engine/native/external/emscripten/physx/physx.$mode.wasm.wasm
-
-echo "|||  FINISH  |||"
-
+#------------------------------------------------------------
+echo -e "\033[01;32m |||  END  |||  \033[0m"
 end_time=$(get_current_time_in_seconds)
-echo -e "\033[01;32m Time Used: "$((end_time-start_time))"s  \033[1m"
-echo -e "\033[01;32m ------------- END -----------------  \033[0m"
-
+echo -e "\033[01;32m Total Time Used: "$((end_time-start_time))"s  \033[1m"
